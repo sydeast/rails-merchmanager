ddb3583aac7f0c0e1333dc95f13f59380de4a51a
Syd <24235464+sydeast@users.noreply.github.com>
2021-03-16 14:56:35 -0500

    "Fix user table passwords"

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_043656) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

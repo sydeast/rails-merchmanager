e937ad3283596081be744e2afcb44beb03cfb476
Syd <24235464+sydeast@users.noreply.github.com>
2021-03-16 19:30:38 -0500

    "Add favorite album to artist"

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
     #Associations
     has_many :artists
     has_many :photocards
-    has_many :albums, through: :artists
+    has_many :albums, {:through=>:artists, :source=>"album"}
 
     #Validations:
-    # has_secure_password
+    has_secure_password
     validates :email, presence: true, uniqueness: true
     validates :name, :password, presence: true
 end

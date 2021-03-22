be9cb944a91ecbe9642f828d2a31a736ab99d57c
Syd <24235464+sydeast@users.noreply.github.com>
2021-03-22 15:52:24 -0500

    "Update home and account setting pages"

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
     def user_post_links
-        if @user.posts.most_comments.empty?
+        if current_user.posts.most_comments.empty?
             tag.p(link_to('No posts yet - write a post here', new_user_post_path(current_user)))
         else
             tag.p(link_to('Create another post', new_user_post_path(current_user)))
@@ -9,7 +9,7 @@ module UsersHelper
     end
 
     def user_photocard_links
-        if @user.photocards.empty?
+        if current_user.photocards.empty?
             tag.p(link_to('No photocards yet - create a photocard here', new_user_photocard_path(current_user)))
         else
              tag.p(link_to('Create more photocards', new_user_photocard_path(current_user)))
@@ -17,7 +17,7 @@ module UsersHelper
     end
 
     def user_artist_links
-        if @user.artists.empty?
+        if current_user.artists.empty?
             tag.p(link_to('No artists yet - create a artist here', new_user_artist_path(current_user)))
         else
             tag.p(link_to('Create more artists', new_user_artist_path(current_user)))
